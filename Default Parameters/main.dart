c4f334b75fb5afeb974d4bf5c9c13df3a267de9b
MuhammadAfra <76904902+MuhammadAfra@users.noreply.github.com>
2022-05-04 13:38:41 +0700

    Week 2

--- Default Parameters/main.dart
+++ Default Parameters/main.dart
@@ -1,8 +1,34 @@
 void main() {
   cariVolume(3, 4,);
+  // cariLuasPersegiPanjang(3, 5);
+  // namaKota("jakarta", "Bandung", "Malang");
+  // int keliling = cariKelilingPersegiPanjang(4, 7);
+  // print(keliling);
+  // usahaAku();
+  // String usahaAkuu = usahaAku();
+  // print(usahaAkuu);
 }
 
 void cariVolume(int panjang, int lebar, {int tinggi:3}) {
   print("Hasil Volume nya adalah ${panjang*lebar*tinggi}");
   print(tinggi);
 }
+
+// void cariLuasPersegiPanjang(int panjang, int lebar) {
+//   print("Hasil Luas Persegi Panjang adalah ${panjang * lebar}");
+// }
+
+// void namaKota(String kota1, String kota2, String kota3) {
+//   print(kota1);
+//   print(kota2);
+//   print(kota3);
+// }
+
+// int cariKelilingPersegiPanjang(int panjang, int lebar) {
+//   int keliling = panjang * lebar ;
+//   return keliling;
+// }
+
+// String usahaAku() {
+//   return "PT>AFRA";
+// }
